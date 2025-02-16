88fc9f9eaf8c0e124f1c494279f3b421c1747813
Aditya Bhatt <96762636+AdityaBhatt3010@users.noreply.github.com>
2025-02-16 12:44:29 +0530

    File Upload

---
+++ src/main/java/C/Hash_MD5.java
@@ -0,0 +1,36 @@
+package C;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+//32 hexadecimal digits
+public class Hash_MD5
+{
+    public static String md5Hash(String input)
+    {
+        try
+        {
+            MessageDigest digest = MessageDigest.getInstance("MD5");
+            byte[] hashBytes = digest.digest(input.getBytes());
+            StringBuilder hexString = new StringBuilder();
+            for (byte b : hashBytes)
+            {
+                hexString.append(String.format("%02x", b));
+            }
+            return hexString.toString();
+        }
+        catch (NoSuchAlgorithmException e)
+        {
+            e.printStackTrace();
+            return "";
+        }
+    }
+
+/*
+    public static void main(String[] args)
+    {
+        String str = "Test";
+        System.out.println(md5Hash(str));
+    }
+*/
+}
---
+++ src/main/java/C/Hash_SHA256.java
@@ -0,0 +1,36 @@
+package C;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+//64 len
+public class Hash_SHA256
+{
+    public static String sha256Hash(String input)
+    {
+        try
+        {
+            MessageDigest digest = MessageDigest.getInstance("SHA-256");
+            byte[] hashBytes = digest.digest(input.getBytes());
+            StringBuilder hexString = new StringBuilder();
+            for (byte b : hashBytes)
+            {
+                hexString.append(String.format("%02x", b));
+            }
+            return hexString.toString();
+        }
+        catch (NoSuchAlgorithmException e)
+        {
+            e.printStackTrace();
+            return "";
+        }
+    }
+
+    /*
+    public static void main(String[] args)
+    {
+        String str = "Test";
+        System.out.println(sha256Hash(str));
+    }
+    */
+}
